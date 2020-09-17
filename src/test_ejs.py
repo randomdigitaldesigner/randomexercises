11c627c2e7b3b87d6075493f0616f8fbd07f675e
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-09-17 19:58:21 -0300

    src/test_ejs.py: add ej2 to tests

--- src/test_ejs.py
+++ src/test_ejs.py
@@ -1,12 +1,20 @@
 from nmigen_cocotb import run
-from ej1 import Suma6
 import pytest
 
 
 @pytest.mark.parametrize("width", [8, 16])
-def test_suma6(width):
+def test_ej1(width):
+    from ej1 import Suma6
     core = Suma6(width)
     ports = core.inputs
     ports.append(core.output)
     run(core, 'tb_ej1', ports=ports, vcd_file='ej1.vcd')
+
+
+@pytest.mark.parametrize("width_in, width_out", [(10, 7)])
+def test_ej2(width_in, width_out):
+    from ej2 import Truncador
+    core = Truncador(width_in=width_in, width_out=width_out)
+    ports = [core.input, core.output]
+    run(core, 'tb_ej2', ports=ports, vcd_file='ej2.vcd')
 
