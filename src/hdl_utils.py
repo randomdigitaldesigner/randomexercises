db5374a415ac0f0003cdd07481f30fa9d26dba36
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-09-17 19:57:59 -0300

    src/hdl_utils.py: int/twos_comp conversion

---
+++ src/hdl_utils.py
@@ -0,0 +1,15 @@
+def twos_comp_from_int(val, bits):
+    """compute the 2's complement of int value val"""
+    assert val >= -2**(bits-1), f'{val} < -2**({bits}-1)'
+    assert val < 2**(bits-1), f'{val} > 2**({bits}-1)'
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val & (2**bits-1)           # return positive value as is
+
+
+def int_from_twos_comp(binary, bits):
+    assert binary & (2**bits - 1) == binary, f'0b{binary:0b} & (2**{bits} - 1) == 0b{binary:0b}'
+    val = binary & ~(2**(bits - 1)) 
+    if binary & 2**(bits - 1): 
+        val -= 2**(bits - 1) 
+    return val
