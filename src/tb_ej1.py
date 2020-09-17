db4f29ea9064520e391a9e3fbe86dc8203ad38d2
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-09-17 19:57:34 -0300

    src/tb_ej1.py: simplified code

--- src/tb_ej1.py
+++ src/tb_ej1.py
@@ -1,7 +1,5 @@
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
-from cocotb.clock import Clock
-from cocotb.regression import TestFactory as tf
+from cocotb.triggers import Timer
 
 import pytest
 import random
@@ -17,23 +15,12 @@ def set_inputs(dut, data):
     dut.input_5 <= data[5]
 
 
-async def init_test(dut):
-    set_inputs(dut, [0 for _ in range(6)])
-    if hasattr(dut, 'clk'):
-        cocotb.fork(Clock(dut.clk, 10, 'ns').start())
-        await RisingEdge(dut.clk)
-        dut.rst <= 1
-        await RisingEdge(dut.clk)
-        dut.rst <= 0
-    else:
-        await Timer(1, units='ns')
-
-
 @cocotb.test()
 async def adder6(dut):
     width = len(dut.input_0)
     length = 10
-    await init_test(dut)
+    
+    set_inputs(dut, [0 for _ in range(6)])
     for i in range(length):
         data = [random.getrandbits(width) for _ in range(6)]
         expected = sum(data)
