6251016f9088f2f542152a37a3020d0785036b67
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-09-19 23:22:10 -0300

    src/tb_ej2.py: fix test

--- src/tb_ej2.py
+++ src/tb_ej2.py
@@ -11,13 +11,13 @@ from math import floor
 async def trunc(dut):
     width_in = len(dut.input)
     width_out = len(dut.output)
-    shift = width_out - width_in
+    shift = width_in - width_out
 
     dut.input <= 0
     for i in range(2**width_in):
         dut.input <= i
         di_signed = int_from_twos_comp(i, width_in)
-        expected = floor(di_signed / 8) # or: di_signed >> shift
+        expected = floor(di_signed / 2**shift) # or: di_signed >> shift
         await Timer(1, units='ns')
         result = dut.output.value.signed_integer
         assert result == expected, f'{result} != {expected}'
