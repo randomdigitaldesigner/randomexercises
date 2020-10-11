9e9309530f533cf5bb12f6120ba2150713efa49c
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-10-11 13:33:20 -0300

    src/hdl_utils.py: shape_limits and add_debug_signal

--- src/hdl_utils.py
+++ src/hdl_utils.py
@@ -13,3 +13,22 @@ def int_from_twos_comp(binary, bits):
     if binary & 2**(bits - 1): 
         val -= 2**(bits - 1) 
     return val
+
+
+def shape_limits(shape):
+    """
+    shape contains elements signed and width
+    """
+    if shape.signed:
+        _min = -2**(shape.width - 1)
+        _max = 2**(shape.width - 1) - 1
+    else:
+        _min = 0
+        _max = 2**shape.width - 1
+    return (_min, _max)
+
+
+def add_debug_signal(signal, name):
+    from nmigen import Signal
+    x = Signal.like(signal, name=name)
+    return x.eq(signal)
