e247dd3b80d5f3bff7579a968f1a6fe07161b3a1
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-10-10 22:18:15 -0300

    src/test_verilog.py: test verilog generation

---
+++ src/test_verilog.py
@@ -0,0 +1,33 @@
+import pytest
+import os
+import shutil
+import shlex
+from ej1 import main as ej1_main
+from ej2 import main as ej2_main
+from ej3 import main as ej3_main
+from ej4 import main as ej4_main
+from ej5 import main as ej5_main
+
+verilog_dir = 'verilogs'
+
+def setup_function(function):
+    if not os.path.exists(verilog_dir):
+        os.makedirs(verilog_dir)
+
+
+def teardown_function(function):
+    pass
+
+
+@pytest.mark.parametrize("main, name", [(ej1_main, 'ej1'),
+                                        (ej2_main, 'ej2'),
+                                        (ej3_main, 'ej3'),
+                                        (ej4_main, 'ej4'),
+                                        (ej5_main, 'ej5')
+                                        ])
+def test_generate_ej(main, name):
+    filename = verilog_dir + '/' + name + '.v'
+    shutil.rmtree(filename, ignore_errors=True)
+    main(shlex.split(filename))
+    assert os.path.exists(filename), f'Failed to generate {filename}'
+
