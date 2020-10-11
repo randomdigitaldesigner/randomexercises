eb3d7af500a19f83f4962e036d1de16ff3ab1180
randomdigitaldesigner <randomdigitaldesigner@gmail.com>
2020-10-10 22:07:54 -0300

    src/ej2.py: main() and argparse

--- src/ej2.py
+++ src/ej2.py
@@ -18,10 +18,23 @@ class Truncador(Elaboratable):
         return m
 
 
-# si llamas este script, genera el verilog para el caso de 8 bits
-if __name__ == '__main__':
+def main(cmd_args=None):
+    import argparse
     from nmigen.back import verilog
-    top = Truncador(width_in=10, width_out=7)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--width-in', '-wi', type=int, default=10,
+                        help='Input port width')
+    parser.add_argument('--width-out', '-wo', type=int, default=7,
+                        help='Output port width')
+    parser.add_argument('output', type=str, help='Output file (Verilog)')
+    args = parser.parse_args(cmd_args)
+    filename = args.output if args.output.endswith('.v') else args.output + '.v'
+    top = Truncador(width_in=args.width_in,
+                    width_out=args.width_out)
     ports = [top.input, top.output]
-    with open("trunc.v", "w") as f:
+    with open(filename, "w") as f:
         f.write(verilog.convert(top, ports=ports))
+
+
+if __name__ == '__main__':
+    main()
